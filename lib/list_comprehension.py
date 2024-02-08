e400860010ec90fa14453844b9531314c4b3e049
melindamackenzie <mem167@pitt.edu>
2024-02-08 11:43:58 -0500

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_elements= [num for num in num_list if num %2 ==0]
+    return even_elements
 
 def make_exclamation(sentence_list):
-    pass
+    sentences_with_exclamation = [sentence + '!' for sentence in sentence_list]
+    return sentences_with_exclamation
